1c8472af42166708aa911bc4ab45f866343e0e3a
Dre Vivero <dre.vivero@gmail.com>
2019-12-28 18:50:40 -0600

    created MainMenu Scene

---
+++ RAIN/Assets/Scipts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Option_Menu()
+    {
+        SceneManager.LoadScene("Options_scene");
+    }
+
+    public void Quit_App()
+    {
+        Debug.Log("Exiting App!");
+        Application.Quit();
+    }
+
+}
